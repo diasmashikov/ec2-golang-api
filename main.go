4df4b771a4e90778b55bf0be45d2b58cfc494920
Dias Mashikov <diasmashikovnasa@gmail.com>
2023-04-24 21:22:57 +0600

    PostgreSQL foundation

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type course struct {
+    ID          int    `json:"id"`
+    Name        string `json:"name"`
+    Description string `json:"description"`
+}
+
 
 func main() {
     // Create a new Gin router
     r := gin.Default()
+
+    // Create a new database connection
+    db, err := sql.Open("postgres", "postgres://ec2admin:ec2mylove@localhost:5432/courses?sslmode=disable")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Test the database connection
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
 
     // Define a route
     r.GET("/", func(c *gin.Context) {
@@ -14,9 +41,24 @@ func main() {
     })
 
 	r.GET("/getCourses", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Here is your IELTS course",
-        })
+        // Execute a SQL query and return the result as JSON
+        rows, err := db.Query("SELECT * FROM course_details")
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        courses := []course{}
+        for rows.Next() {
+            var course course
+            err := rows.Scan(&course.ID, &course.Name, &course.Description)
+            if err != nil {
+                log.Fatal(err)
+            }
+            courses = append(courses, course)
+        }
+
+        c.JSON(http.StatusOK, courses)
     })
 
     // Start the server
